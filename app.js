814f59836a083f14de7dcc801254f2419c617064
GiorgosDiamadis <56475498+GiorgosDiamadis@users.noreply.github.com>
2021-03-11 01:26:37 +0200

    11/3/21

--- app.js
+++ app.js
@@ -1,146 +1,70 @@
 const express = require("express");
-const path = require("path");
-const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
-const Campground = require("./models/campground");
-const Review = require("./models/reviews");
-const catchAsync = require("./utils/catchAsync");
+const mongoose = require("mongoose");
+const path = require("path");
+const session = require("express-session");
 const ExpressError = require("./utils/ExpressError");
-const Joi = require("joi");
-const campgroundSchema = require("./schemas");
+const campgroundRouters = require("./routes/campgroundRoutes");
+const reviewRouters = require("./routes/reviewRoutes");
+const flash = require("connect-flash");
+const { RSA_NO_PADDING } = require("constants");
 
-const validateCampground = (req, res, next) => {
-  const { error } = campgroundSchema.validate(req.body);
+function init_database() {
+  mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  });
 
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(msg, 400);
-  }
-};
+  const db = mongoose.connection;
+  db.on("error", console.error.bind(console, "connection error:"));
+  db.once("open", () => {
+    console.log("Database connected");
+  });
+  return db;
+}
 
-mongoose.connect("mongodb://localhost:27017/yelp-camp", {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
+function init_express() {
+  const app = express();
+  app.engine("ejs", ejsMate);
+  app.set("view engine", "ejs");
+  app.set("views", path.join(__dirname, "views"));
+  app.use(express.urlencoded({ extended: true }));
+  app.use(methodOverride("_method"));
+  app.use(express.static(__dirname + "/public"));
+  app.use(flash());
+  const sessionConfig = {
+    secret: "giorgos",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+      expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+  };
+  app.use(session(sessionConfig));
+  return app;
+}
+
+const db = init_database();
+const app = init_express();
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
 });
 
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-  console.log("Database connected");
-});
+app.use("/campgrounds", campgroundRouters);
 
-const app = express();
-
-app.engine("ejs", ejsMate);
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
-app.use(express.static(__dirname + "/public"));
+app.use("/campgrounds/:id/review", reviewRouters);
 
 app.get("/", (req, res) => {
   res.render("home");
 });
-
-app.get(
-  "/campgrounds",
-  catchAsync(async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render("campgrounds/index", { campgrounds });
-  })
-);
-
-app.get("/campgrounds/new", (req, res) => {
-  res.render("campgrounds/new");
-});
-
-// When creating the campground
-app.post(
-  "/campgrounds",
-  validateCampground,
-  catchAsync(async (req, res, next) => {
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-  })
-);
-
-app.get(
-  "/campgrounds/:id",
-  catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate(
-      "reviews"
-    );
-    res.render("campgrounds/show", { campground });
-  })
-);
-
-app.get(
-  "/campgrounds/:id/edit",
-  catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    res.render("campgrounds/edit", { campground });
-  })
-);
-
-app.put(
-  "/campgrounds/:id",
-  validateCampground,
-  catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {
-      ...req.body.campground,
-    });
-    res.redirect(`/campgrounds/${campground._id}`);
-  })
-);
-
-app.get(
-  "/campgrounds/:id/review",
-  catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id);
-    res.render("reviews/review", { campground });
-  })
-);
-
-app.post(
-  "/campgrounds/:id/review",
-  catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id);
-    const review = new Review(req.body.rev);
-    campground.reviews.push(review);
-
-    await review.save();
-    await campground.save();
-
-    res.redirect(`/campgrounds/${id}`);
-  })
-);
-
-app.delete(
-  "/campgrounds/:id",
-  catchAsync(async (req, res) => {
-    const { id } = req.params;
-    await Campground.findByIdAndDelete(id);
-    res.redirect("/campgrounds");
-  })
-);
-app.delete(
-  "/campgrounds/:id/review/:reviewId",
-  catchAsync((req, res, next) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews:reviewId }} );
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/campgrounds/${id}`);
-
-  })
-);
-
 app.all("*", (req, res, next) => {
   next(new ExpressError("Page Not Found", 404));
 });
--- validation.js
+++ validation.js
@@ -1,11 +1,18 @@
 const Joi = require("joi");
 
-module.exports = campgroundSchema = Joi.object({
+module.exports.campgroundSchema = Joi.object({
   campground: Joi.object({
     title: Joi.string().required(),
     image: Joi.string().required(),
     price: Joi.number().required().min(0),
     description: Joi.string().required(),
     location: Joi.string().required(),
+  }).required(),
+});
+
+module.exports.reviewValidation = Joi.object({
+  rev: Joi.object({
+    review: Joi.string().required(),
+    rating: Joi.number().min(1).max(5).required(),
   }).required(),
 });
